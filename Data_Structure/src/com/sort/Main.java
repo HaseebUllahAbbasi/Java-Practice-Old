70aac95be04556c0cea5cf746acd324f2ddf0310
Haseeb Ullah <haseebabbasi00@gmail.com>
2020-09-24 10:18:48 +0500

    Mean Median Added

--- Data_Structure/src/com/sort/Main.java
+++ Data_Structure/src/com/sort/Main.java
@@ -4,10 +4,10 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int array [] ={23,44,12,15,3,1,45};
+        int array [] ={2,44,12,15,3,1,45};
         for(int i=1; i< array.length; i++)
         {
-
+            int a=0;
             while (i>0 && array[i]<array[i-1])
             {
                 int temp = array[i];
@@ -16,9 +16,6 @@ public class Main {
                 i--;
             }
         }
-       // for(int i=0; i< array.length; i++)
-       // {
             System.out.println(Arrays.toString(array));
-        //}
     }
 }
