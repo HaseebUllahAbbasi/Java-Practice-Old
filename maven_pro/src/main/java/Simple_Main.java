0a2ca318a933ce486c64ce4f8a1fc212549732c4
Haseeb Ullah <haseebabbasi00@gmail.com>
2020-12-14 13:32:59 +0500

    Json dependency added

--- maven_pro/src/main/java/Simple_Main.java
+++ maven_pro/src/main/java/Simple_Main.java
@@ -1,3 +1,5 @@
+import jdk.nashorn.internal.parser.JSONParser;
+
 public class Simple_Main
 {
     public static void main(String[] args)
@@ -12,5 +14,6 @@ class Simple implements Simple_Interface
     @Override
     public void call_me_method() {
         System.out.println("call me method is called of the interface");
+
     }
 }
